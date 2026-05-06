21ea09bb5d04ef06fd0babfbcf74ff73ff4b878d
biama25 <psxba9@nottingham.ac.uk>
2026-05-07 00:15:52 +0100

    Add files via upload

---
+++ src/data/generated/bib.js
@@ -0,0 +1,111 @@
+﻿const generatedBibEntries = {
+        "dixon2019": {
+        "author": "Dixon, Matthew and Arachchilage, Nalin Asanka Gamagedara and Nicholson, James",
+        "booktitle": "Extended Abstracts of the CHI Conference on Human Factors in Computing Systems",
+        "doi": "10.1145/3290607.3313026",
+        "keywords": "type:user_study, audience:young_adults, topic:phishing, topic:game_based_learning",
+        "title": "Engaging Users with Educational Games: The Case of Phishing",
+        "type": "inproceedings",
+        "year": "2019"
+    },
+    "furnell2017": {
+        "author": "Furnell, Steven and Vasileiou, Ioannis",
+        "doi": "10.1016/S1353-4858(17)30122-8",
+        "journal": "Network Security",
+        "keywords": "type:framework, audience:employees, topic:personalisation, topic:awareness_training",
+        "number": "12",
+        "pages": "5--9",
+        "title": "Security Education and Awareness: Just Let Them Burn?",
+        "type": "article",
+        "volume": "2017",
+        "year": "2017"
+    },
+    "furnell2026": {
+        "address": "Perth, Australia",
+        "author": "Furnell, Steven and Todd, James and Smid, Lucija and Castle-Green, Simon and Carpent, Xavier",
+        "booktitle": "Proceedings of the IFIP World Conference on Information Security Education (WISE 2026)",
+        "keywords": "type:physical_game, audience:general_public, topic:game_based_learning, topic:awareness_training",
+        "title": "Provoking Interest in Cybersecurity Using Physical Games",
+        "type": "inproceedings",
+        "year": "2026"
+    },
+    "herkanaidu2021": {
+        "author": "Herkanaidu, Ram and Furnell, Steven M. and Papadaki, Maria",
+        "doi": "10.1108/ICS-11-2020-0183",
+        "journal": "Information and Computer Security",
+        "keywords": "type:framework, audience:children, topic:cultural_adaptation, topic:online_safety",
+        "number": "4",
+        "pages": "664--679",
+        "title": "Towards a Cross-Cultural Education Framework for Online Safety Awareness",
+        "type": "article",
+        "volume": "29",
+        "year": "2021"
+    },
+    "khern2023": {
+        "author": "Khern-am-nuai, Warut and Hashim, Muhammad Junaid and Pinsonneault, Alain and Yang, Wei and Li, Ning",
+        "doi": "10.1287/isre.2022.1125",
+        "journal": "Information Systems Research",
+        "keywords": "type:tool, audience:general_public, topic:password_security, topic:persuasion",
+        "number": "1",
+        "pages": "157--177",
+        "title": "Augmenting Password Strength Meter Design Using the Elaboration Likelihood Model",
+        "type": "article",
+        "volume": "34",
+        "year": "2023"
+    },
+    "ng2025": {
+        "author": "Ng, Chin Yee and Hasan, Mohammad Kamrul Bin",
+        "doi": "10.1016/j.cose.2024.104307",
+        "journal": "Computers and Security",
+        "keywords": "type:systematic_review, audience:general_public, topic:serious_games, topic:game_based_learning",
+        "pages": "104307",
+        "title": "Cybersecurity Serious Games Development: A Systematic Review",
+        "type": "article",
+        "volume": "150",
+        "year": "2025"
+    },
+    "saglam2023": {
+        "author": "Saglam, Rahime Belen and Miller, Virginia and Franqueira, Virginia N. L.",
+        "doi": "10.1109/TE.2022.3231019",
+        "journal": "IEEE Transactions on Education",
+        "keywords": "type:systematic_review, audience:children, topic:curriculum, topic:online_safety",
+        "number": "3",
+        "pages": "274--286",
+        "title": "A Systematic Literature Review on Cyber Security Education for Children",
+        "type": "article",
+        "volume": "66",
+        "year": "2023"
+    },
+    "spatafora2024": {
+        "author": "Spatafora, Andrea and Wagemann, Markus and Sandoval, Carlos and Leisenberg, Markus and Vaz de Carvalho, Carlos",
+        "doi": "10.3390/computers13080205",
+        "journal": "Computers",
+        "keywords": "type:escape_room, audience:employees, topic:serious_games, topic:SME",
+        "number": "8",
+        "pages": "205",
+        "title": "An Educational Escape Room Game to Develop Cybersecurity Skills",
+        "type": "article",
+        "volume": "13",
+        "year": "2024"
+    },
+    "weeratunge2026": {
+        "author": "Weeratunge, Nadeesha H. and Hjelsvold, Rune and Katt, Basel",
+        "doi": "10.1007/s10207-026-01239-9",
+        "journal": "International Journal of Information Security",
+        "keywords": "type:systematic_review, audience:general_public, topic:game_based_learning, topic:evaluation",
+        "title": "A Systematic Scoping Review on Game-Based Cybersecurity Awareness Education",
+        "type": "article",
+        "year": "2026"
+    },
+    "yasin2026": {
+        "author": "Yasin, Affan and Fatima, Ramsha and Puspitasari, Ika and Dogar, Amjad Bashir and Yang, Liu and Ishaq, Kashif",
+        "doi": "10.1007/s10207-025-01193-y",
+        "journal": "International Journal of Information Security",
+        "keywords": "type:simulation, audience:students, topic:social_engineering, topic:game_based_learning",
+        "pages": "32",
+        "title": "From Prey to Protect: Exploring Social Engineering Through Games and Simulation",
+        "type": "article",
+        "volume": "25",
+        "year": "2026"
+    }
+};
